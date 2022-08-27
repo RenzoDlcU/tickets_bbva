79cad4a5c18d699b2eb3e7c027fe86a3c2987fcf
Renzo De La Cruz <renzo99_0@hotmail.com>
2022-08-27 11:43:07 -0500

    adding deploy config

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
